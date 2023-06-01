20c31c92d5bef0ed6e4ca556aa4645df20416281
tanmaypatidar29 <96785905+tanmaypatidar29@users.noreply.github.com>
2023-06-01 23:36:42 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Pattern 6/pattern-6.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code here
+	    for(int row = 1;row<=n;row++)
+	    {
+	        for(int col = 1;col<=n-(row-1);col++)
+	        {
+	            cout << col << " ";
+	        }
+	        cout << endl;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
