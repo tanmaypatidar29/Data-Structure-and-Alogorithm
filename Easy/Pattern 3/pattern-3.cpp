a9eae7ca937c93581c7ce58cb8d83e563c10adca
tanmaypatidar29 <96785905+tanmaypatidar29@users.noreply.github.com>
2023-06-01 23:21:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 3/pattern-3.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int row = 1;row<=n;row++)
+        {
+            for(int col = 1;col<=row;col++)
+            {
+                cout << col <<" ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
